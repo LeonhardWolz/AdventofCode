02eaacd58ef4bcb0c5466fe9f8b230034133574d
= <leonhard.wolz@gmail.com>
2023-12-02 14:05:20 +0100

    Second Challenge

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -4,24 +4,26 @@ import re
 sum = 0
 with open("Day_2/input.txt") as f:
     for line in f:
-        valid = True
+        max_red = 0
+        max_green = 0
+        max_blue = 0
+
         line = re.split(r":|,|;", line)
         for token in line:
             if re.search(r"red", token):
                 print(token)
                 count = int(re.sub(r"\D", r"", token))
-                if(count > 12):
-                    valid = False
+                max_red = count if count > max_red else max_red
+
             elif re.search(r"green", token):
                 count = int(re.sub(r"\D", r"", token))
-                if(count > 13):
-                    valid = False
+                max_green = count if count > max_green else max_green
+
             elif re.search(r"blue", token):
                 count = int(re.sub(r"\D", r"", token))
-                if(count > 14):
-                    valid = False
+                max_blue = count if count > max_blue else max_blue
         
-        if valid:
-            sum += int(re.sub(r"\D", r"", line[0]))
+        sum += max_red*max_green*max_blue
+        
 
 print(sum)

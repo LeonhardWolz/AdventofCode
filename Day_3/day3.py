56faf0022190355e69199d43f1f276288160f5dc
= <leonhard.wolz@gmail.com>
2023-12-03 15:27:53 +0100

    Second Challenge Day3

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -4,21 +4,24 @@ sum = 0
 with open("Day_3/input.txt") as f:
     content = f.read().split("\n")
     for index_l, line in enumerate(content):
-        for match_obj in re.finditer(r"(\d+)",line):
-            neighbours = ""
+        for match_obj in re.finditer(r"(\*)",line):
+            neighbours = []
             if index_l > 0:
-                neighbours += content[index_l-1][max(match_obj.start()-1, 0):min(match_obj.end()+1,len(line))]
+                for number_match in re.finditer(r"(\d+)", content[index_l-1]):
+                    if match_obj.start()-1 <= number_match.start() <= match_obj.end() or match_obj.start()-1 <= number_match.end()-1 <= match_obj.end():
+                        neighbours.append(number_match.group())
+                        
             if index_l < len(content)-1:
-                neighbours += content[index_l+1][max(match_obj.start()-1, 0):min(match_obj.end()+1,len(line))]
+                for number_match in re.finditer(r"(\d+)", content[index_l+1]):
+                    if match_obj.start()-1 <= number_match.start() <= match_obj.end() or match_obj.start()-1 <= number_match.end()-1 <= match_obj.end():
+                        neighbours.append(number_match.group())
             
-            if match_obj.start() > 0:
-                neighbours += line[match_obj.start()-1]
-            if match_obj.end() < len(line):
-                neighbours += line[match_obj.end()]
-
-            if re.search(r"([^\d.])", neighbours):
-                sum += int(match_obj.group())
-
+            for number_match in re.finditer(r"(\d+)", line):
+                if number_match.end() == match_obj.start() or number_match.start() == match_obj.end():
+                    neighbours.append(number_match.group())
+            
+            if len(neighbours) == 2:
+                sum += int(neighbours[0])*int(neighbours[1])
 
 print(sum)
 
